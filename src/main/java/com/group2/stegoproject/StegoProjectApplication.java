9f5806f9462950cb4da3fe608fc1f89a6593d9cc
Adrian Perez Jr <adrianperezjr@gmail.com>
2019-03-19 13:25:54 -0500

    New Java Project

---
+++ src/main/java/com/group2/stegoproject/StegoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.group2.stegoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StegoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StegoProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group2/stegoproject/StegoProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.group2.stegoproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StegoProjectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
